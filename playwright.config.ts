21758c5db7b6d832e34e5d226e58c96625fc8a1a
giangnguyenKMS <giangtnguyen@kms-technology.com>
2024-09-08 17:53:48 +0700

    Adding test cases demo

---
+++ playwright.config.ts
@@ -0,0 +1,57 @@
+import { defineConfig, devices } from '@playwright/test';
+
+export default defineConfig({
+  testDir: './tests',
+  fullyParallel: true,
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 2 : 0,
+  workers: process.env.CI ? 1 : 1,
+  reporter: 'html',
+  use: {
+    trace: 'on',
+    headless: true,
+    // storageState: './auth.json',
+  },
+  // globalSetup: './tests/02-demo-setup-teardown/global-setup.ts',
+  // globalTeardown: './tests/02-demo-setup-teardown/global-teardown.ts',
+
+  /* Configure projects for major browsers */
+  projects: [
+    // {
+    //   name: 'chromium',
+    //   use: { ...devices['Desktop Chrome'] },
+    // },
+
+    // {
+    //   name: 'firefox',
+    //   use: { ...devices['Desktop Firefox'] },
+    // },
+
+    // {
+    //   name: 'webkit',
+    //   use: { ...devices['Desktop Safari'] },
+    // },
+
+
+    // Dependencies setup/teardown --------------------------------------------------
+    {
+      name: "setup",
+      // testDir: "./tests/02-demo-setup-teardown",
+      testDir: "./tests/03-demo-pom-fixture",
+      testMatch: "dependencies-setup.ts",
+      teardown: "teardown",
+    },
+    {
+      name: "teardown",
+      // testDir: "./tests/02-demo-setup-teardown",
+      testDir: "./tests/03-demo-pom-fixture",
+      testMatch: "dependencies-teardown.ts",
+    },
+    {
+      name: 'chromium',
+      dependencies: ["setup"],
+      use: { ...devices['Desktop Chrome'], storageState: "./auth.json" },
+    },
+    // ------------------------------------------------------------------------
+  ],
+});
---
+++ tests/02-demo-setup-teardown/test/dependencies-teardown.ts
@@ -0,0 +1,5 @@
+import { test } from "@playwright/test";
+
+test("Clean up", async () => {
+  console.log("Perform clear up steps......for test case")
+});
